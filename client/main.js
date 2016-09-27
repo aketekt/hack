665136ca8a11b176869d8ba428e431b817af302d
Will <will.sch@protonmail.com>
2016-09-28 00:08:20 +0200

    change to blue style

--- client/main.js
+++ client/main.js
@@ -2,3 +2,7 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
 import { name as Outr } from '../imports/ui/components/outr/outr';
+
+Router.route('/', function () {
+  this.render('Home');
+});
